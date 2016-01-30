487ffd7a8c1acc70e0cf46092af3966fff388859
Yifei Teng <teng9@illinois.edu>
2016-01-30 14:44:51 -0600

    Add more projects

---
+++ Pitcher/Pitcher/Pitcher.h
@@ -0,0 +1,19 @@
+//
+//  Pitcher.h
+//  Pitcher
+//
+//  Created by Yifei Teng on 1/30/16.
+//  Copyright © 2016 Takefive Interactive. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Pitcher.
+FOUNDATION_EXPORT double PitcherVersionNumber;
+
+//! Project version string for Pitcher.
+FOUNDATION_EXPORT const unsigned char PitcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pitcher/PublicHeader.h>
+
+
---
+++ Scoresmith/Scoresmith/Scoresmith.h
@@ -0,0 +1,19 @@
+//
+//  Scoresmith.h
+//  Scoresmith
+//
+//  Created by Yifei Teng on 1/30/16.
+//  Copyright © 2016 Takefive Interactive. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Scoresmith.
+FOUNDATION_EXPORT double ScoresmithVersionNumber;
+
+//! Project version string for Scoresmith.
+FOUNDATION_EXPORT const unsigned char ScoresmithVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Scoresmith/PublicHeader.h>
+
+
