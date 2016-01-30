d77ce2a00f7678a1ff694b933ef7f7d094f144a9
Yu Wang <yuwang17@illinois.edu>
2016-01-30 14:10:36 -0600

    init OS X and workspace

---
+++ Singularity-Desktop/Singularity-Desktop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Singularity-Desktop
+//
+//  Created by Wang Yu on 1/30/16.
+//  Copyright © 2016 Takefive Interactive. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Singularity-Desktop/Singularity-Desktop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Singularity-Desktop
+//
+//  Created by Wang Yu on 1/30/16.
+//  Copyright © 2016 Takefive Interactive. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
