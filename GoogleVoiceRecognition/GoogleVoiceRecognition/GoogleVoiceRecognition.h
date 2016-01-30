24362d933e7105c37c17e980dd2491f7c5311816
Yifei Teng <teng9@illinois.edu>
2016-01-30 14:23:21 -0600

    Add Google Voice Recognition Scaffold

---
+++ GoogleVoiceRecognition/GoogleVoiceRecognition/GoogleVoiceRecognition.h
@@ -0,0 +1,19 @@
+//
+//  GoogleVoiceRecognition.h
+//  GoogleVoiceRecognition
+//
+//  Created by Yifei Teng on 1/30/16.
+//  Copyright © 2016 Yifei Teng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GoogleVoiceRecognition.
+FOUNDATION_EXPORT double GoogleVoiceRecognitionVersionNumber;
+
+//! Project version string for GoogleVoiceRecognition.
+FOUNDATION_EXPORT const unsigned char GoogleVoiceRecognitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleVoiceRecognition/PublicHeader.h>
+
+
