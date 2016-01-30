1795e551a78fa9ac5c463f6ffb707f910df15007
Yifei Teng <teng9@illinois.edu>
2016-01-30 14:25:07 -0600

    Add hyphenator project scaffolding

---
+++ Hyphenator/Hyphenator/Hyphenator.h
@@ -0,0 +1,19 @@
+//
+//  Hyphenator.h
+//  Hyphenator
+//
+//  Created by Yifei Teng on 1/30/16.
+//  Copyright © 2016 Takefive Interactive. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Hyphenator.
+FOUNDATION_EXPORT double HyphenatorVersionNumber;
+
+//! Project version string for Hyphenator.
+FOUNDATION_EXPORT const unsigned char HyphenatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hyphenator/PublicHeader.h>
+
+
