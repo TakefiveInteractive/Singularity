85b52ac306a8efc346db05c3da4fe992d60253f1
Yifei Teng <teng9@illinois.edu>
2016-01-31 18:49:34 -0600

    Install SwiftHTTP

---
+++ Pods/Target Support Files/Pods-GoogleVoiceRecognition/Pods-GoogleVoiceRecognition-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoogleVoiceRecognition : NSObject
+@end
+@implementation PodsDummy_Pods_GoogleVoiceRecognition
+@end
---
+++ Pods/Target Support Files/SwiftHTTP/SwiftHTTP-umbrella.h
@@ -0,0 +1,6 @@
+#import <Cocoa/Cocoa.h>
+
+
+FOUNDATION_EXPORT double SwiftHTTPVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftHTTPVersionString[];
+
