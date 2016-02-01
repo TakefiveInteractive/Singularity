a4081c2eba117741c65b1310658973cf539c3b4c
Yifei Teng <teng9@illinois.edu>
2016-01-31 22:22:28 -0600

    Add FLAC library

---
+++ GoogleVoiceRecognition/Source/VoiceRecognition.swift
@@ -0,0 +1,19 @@
+import SwiftHTTP
+
+public enum VoiceLanguages : String {
+    case English = "en_US"
+    case Mandarin = "zh_CN"
+    case Japanese = "jp_JP"
+}
+
+private let VoiceApiKeys = [
+    "AIzaSyDG6H_g7olZFWdIIWCtWPiYsqSrkWSRyzM",
+    "AIzaSyBLnwjUYipg5mOFxG5bNAPrlvQmroMHvss",
+    "AIzaSyDcaEqYmbfmRtMMSCRRgwYrituNgrhmlsE"
+]
+
+class VoiceRecognition {
+    class func recognize() {
+        
+    }
+}
