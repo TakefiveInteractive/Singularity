5a8c4debd499d230fa3e832858cdbc9c49da6c6c
Yu Wang <yuwang17@illinois.edu>
2016-02-18 23:11:12 -0600

    baisc view layout

--- Singularity-Desktop/Singularity-Desktop/MainViewController.swift
+++ Singularity-Desktop/Singularity-Desktop/MainViewController.swift
@@ -12,6 +12,9 @@ import SnapKit
 class MainViewController: NSViewController {
     
     var effectView: VisualizerView!
+    @IBOutlet weak var headerView: NSView!
+    @IBOutlet weak var backView: NSView!
+    @IBOutlet weak var footerView: NSView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,22 +23,42 @@ class MainViewController: NSViewController {
             $0.height.equalTo(576)
         }
         preferredContentSize = view.fittingSize
+        
+        self.view.window?.styleMask = NSClosableWindowMask | NSTitledWindowMask | NSMiniaturizableWindowMask
+        effectView = VisualizerView(frame: self.view.bounds)
+        effectView.autoresizingMask = [.ViewWidthSizable, .ViewHeightSizable]
+        effectView.layer?.backgroundColor = Palette.blurBackColor
+        self.backView.addSubview(effectView)
+        NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "updateVisualizationView", userInfo: nil, repeats: true)
+        
+        let blurView = NSVisualEffectView(frame: self.view.bounds)
+        blurView.blendingMode = NSVisualEffectBlendingMode.WithinWindow
+        blurView.material = NSVisualEffectMaterial.Light
+        self.backView.addSubview(blurView)
+        
+        self.view.wantsLayer = true
+        self.view.superview?.wantsLayer = true
+        self.headerView.wantsLayer = true
+        
+        self.headerView.shadow = NSShadow()
+        self.headerView.alphaValue = 0.7
+        self.headerView.layer?.shadowOpacity = 0.7
+        self.headerView.layer?.shadowColor = NSColor.blackColor().CGColor
+        self.headerView.layer?.shadowOffset = NSMakeSize(0, 0)
+        self.headerView.layer?.shadowRadius = 3
+        self.headerView.layer?.backgroundColor = Palette.windowColor
+        
+        self.footerView.shadow = NSShadow()
+        self.footerView.alphaValue = 0.7
+        self.footerView.layer?.shadowOpacity = 0.7
+        self.footerView.layer?.shadowColor = NSColor.blackColor().CGColor
+        self.footerView.layer?.shadowOffset = NSMakeSize(0, 0)
+        self.footerView.layer?.shadowRadius = 3
+        self.footerView.layer?.backgroundColor = Palette.windowColor
     }
     
     override func viewDidAppear() {
         super.viewDidAppear()
-        self.view.window?.styleMask = NSClosableWindowMask | NSTitledWindowMask | NSMiniaturizableWindowMask
-        effectView = VisualizerView(frame: self.view.bounds)
-        effectView.autoresizingMask = [.ViewWidthSizable, .ViewHeightSizable]
-        effectView.layer?.backgroundColor = NSColor.clearColor().CGColor
-        self.view.addSubview(effectView)
-        NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "updateVisualizationView", userInfo: nil, repeats: true)
-        
-        let blurView = NSVisualEffectView(frame: self.view.bounds)
-        view.wantsLayer = true
-        blurView.blendingMode = NSVisualEffectBlendingMode.WithinWindow
-        blurView.material = NSVisualEffectMaterial.Light
-        self.view.addSubview(blurView)
         
     }
 
---
+++ Singularity-Desktop/Singularity-Desktop/Palette.swift
@@ -0,0 +1,14 @@
+//
+//  Palette.swift
+//  Singularity-Desktop
+//
+//  Created by Wang Yu on 2/18/16.
+//  Copyright © 2016 Takefive Interactive. All rights reserved.
+//
+
+import Cocoa
+
+struct Palette {
+    static let blurBackColor = NSColor(hexString: "A2DED0")!.CGColor
+    static let windowColor = NSColor(hexString: "65C6BB")!.CGColor
+}
