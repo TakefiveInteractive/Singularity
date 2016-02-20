5fb28584a7a54d2b98f250bea045cd5a2dfddd9a
Yifei Teng <teng9@illinois.edu>
2016-02-20 14:10:23 -0600

    Find best concert tuning

--- Pitcher/Source/Pitcher.swift
+++ Pitcher/Source/Pitcher.swift
@@ -23,6 +23,13 @@ public enum Frequency {
     case VolumeLow
 }
 
+/// Exponentiation operator
+infix operator ** { associativity left precedence 170 }
+
+func ** (num: Float, power: Float) -> Float {
+    return Float(pow(Double(num), Double(power)))
+}
+
 public class NoteEngine {
     
     public init() {
@@ -30,6 +37,36 @@ public class NoteEngine {
     }
     
     public func pitchToNote(pitches: [Frequency], bpm: Float, pitchPerSecond: Float) -> [Note] {
+        // Select best tuning standard frequency
+        // Start from C2
+        
+        // Make a semitone sequence from D2
+        var sequence: Array<Pitch> = []
+        let semitone = Scale.Chromatic
+        for octave: UInt in 2...4 {
+            let D = Pitch(chroma: .D, octave: octave)
+            sequence += Array(semitone(D))
+        }
+        
+        var bestError: Float = 100000.0
+        var bestConcertA = 440
+        for concertA in 430...450 {
+            MusicKit.concertA = Double(concertA)
+            var error: Float = 0.0
+            for case let Frequency.Freq(freq) in pitches {
+                let closestStandardFreq = sequence
+                    .map({ ($0.frequency - freq) ** 2 })
+                    .minElement()!
+                error += closestStandardFreq
+            }
+            if error < bestError {
+                bestError = error
+                bestConcertA = concertA
+            }
+        }
+        print("Best concert pitch: \(bestConcertA)")
+        MusicKit.concertA = Double(bestConcertA)
+        
         return []
     }
 }
